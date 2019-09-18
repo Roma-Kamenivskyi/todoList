40ec5d8483d1f131ebda185fd0138b49c0c3c5cf
Roma Kamenivskiy <jumji19992412@gmail.com>
2019-09-18 22:01:14 +0300

    some changes

--- script.js
+++ script.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-(function() {
-  //DOM vaariables
-  const form = document.querySelector("#form");
-  const taskList = document.getElementById("taskList");
-  const todoInput = document.getElementById("todoInput");
-
-  let arrayOfItems = [];
-  // listeners
-  form.addEventListener("submit", addTaskToList);
-  taskList.addEventListener("click", itemBtnListeners);
-
-  // functions
-  function addTaskToList(e) {
-    e.preventDefault();
-    arrayOfItems.push(todoInput.value);
-    createTodoItems();
-    todoInput.value = "";
-  }
-
-  function createTodoItems() {
-    let items = "";
-    arrayOfItems.forEach(item => {
-      items += `<li class="list-item">
-          <label>${item}</label>
-          <input type="text" class="inputEdit">
-          <div>
-            <button class="item-button edit-btn">Edit</button>
-            <button class="item-button remove-btn">Remove</button>
-          </div>
-        </li>`;
-    });
-    appendItemToList(items);
-  }
-
-  function appendItemToList(items) {
-    taskList.innerHTML = items;
-  }
-
-  function itemBtnListeners(e) {
-    if (e.target.classList.contains("remove-btn")) {
-      e.target.parentElement.parentElement.remove();
-    }
-    if (e.target.classList.contains("edit-btn")) {
-      console.log("Edit");
-    }
-  }
-})();
-=======
 (() => {
   //DOM vaariables
   const form = document.getElementById("form");
@@ -130,4 +81,3 @@
     });
   }
 })();
->>>>>>> b3b414370179b49c8660ec8548be62aefdf2b6c7
